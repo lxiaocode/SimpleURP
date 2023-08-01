cec25cdc456704579d135085537575b3d9fb451d
lxiaocode <lxiaocode@qq.com>
2023-08-01 15:45:28 +0800

    draw Transparent

--- Assets/SimpleURP/SimpleRenderer.cs
+++ Assets/SimpleURP/SimpleRenderer.cs
@@ -7,11 +7,14 @@ namespace SimpleURP
     public class SimpleRenderer : ScriptableRenderer
     {
         SimpleDrawObjectsPass m_RenderOpaqueForwardPass;
+        SimpleDrawObjectsPass m_RenderTransparentForwardPass;
         SimpleDrawSkyboxPass m_DrawSkyboxPass;
         
         public SimpleRenderer(SimpleRendererData data) : base(data)
         {
             m_RenderOpaqueForwardPass = new SimpleDrawObjectsPass(RenderPassEvent.BeforeRenderingOpaques, true, RenderQueueRange.opaque, data.opaqueLayerMask);
+            m_RenderTransparentForwardPass = new SimpleDrawObjectsPass(RenderPassEvent.BeforeRenderingTransparents, false, RenderQueueRange.transparent, data.transparentLayerMask);
+            
             m_DrawSkyboxPass = new SimpleDrawSkyboxPass(RenderPassEvent.BeforeRenderingSkybox);
         }
 
@@ -19,6 +22,7 @@ namespace SimpleURP
         {
             EnqueuePass(m_RenderOpaqueForwardPass);
             EnqueuePass(m_DrawSkyboxPass);
+            EnqueuePass(m_RenderTransparentForwardPass);
         }
     }
 }
